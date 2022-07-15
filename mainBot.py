fdf094cd95791160077c74b1e3176f8cff3000e2
KornoosVildberg <ryzhikov_andrej@list.ru>
2022-07-16 01:46:13 +0300

    1

--- mainBot.py
+++ mainBot.py
@@ -1,3 +1,10 @@
 import telebot
 
-bot = telebot.TeleBot('')
+bot = telebot.TeleBot('5360883455:AAHk4kyImiqL5VS7Vd4Xwz9AmCVubGwfut4')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, '<b>Привет</b>', parse_mode='html')
+
+
+    bot.polling(non_stop=True)
